3185baf48c93d5038b653760bbfff947fd85d388
sbrunner210 <sb6911@stern.nyu.edu>
2021-05-27 18:34:28 -0400

    Cleaning Up

--- game.py
+++ game.py
@@ -1,12 +1,24 @@
 # game.py
 
 import random
+import os
+# from dotenv import load_dotenv
 
+#load_dotenv()
+
+# player_name = os.getenv(user)
+
+# print (player_name)
+print("-----------------------------")
 print("Rock, Paper, Scissors, Shoot!")
+print("-----------------------------")
 
 user_choice = input("Please choose one of 'rock' , 'paper', 'scissors': ")
 
+print("-----------------------------")
 print("USER CHOICE: ",user_choice)
+print("-----------------------------")
+
 
 # validate the input such that only if it is one of the accepted inputs...
 #... we will continue with the rest of the program. Otherwise, we'll stop ...
@@ -14,14 +26,17 @@ print("USER CHOICE: ",user_choice)
 #... to run again
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print ("VALID. KEEP GOING")
+    print ("GOOD CHOICE!")
 else:
     print("OOPS, INVALID INPUT. PLEASE TRY AGAIN.")
     exit()
 
 valid_options = ["rock","paper","scissors"]
 computer_choice = random.choice(valid_options)
+
+print("-----------------------------")
 print("COMPUTER CHOICE: ", computer_choice)
+print("-----------------------------")
 
 if user_choice == computer_choice:
     print ("IT'S A TIE!")
