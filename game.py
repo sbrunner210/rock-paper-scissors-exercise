8da78d390ea7753cdcc7bd4e978d7a485306480b
sbrunner210 <sb6911@stern.nyu.edu>
2021-05-27 10:02:53 -0400

    Game Logic

--- game.py
+++ game.py
@@ -23,4 +23,27 @@ valid_options = ["rock","paper","scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
 
+if user_choice == computer_choice:
+    print ("IT'S A TIE!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print (user_choice, ">", computer_choice, "YOU WIN!")
+    if computer_choice == "paper":
+        print (user_choice, "<", computer_choice, "YOU LOSE! :(")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print (user_choice, ">", computer_choice, "YOU WIN!")
+    if computer_choice == "scissors":
+        print (user_choice, "<", computer_choice, "YOU LOSE! :(")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print (user_choice, ">", computer_choice, "YOU WIN!")
+    if computer_choice == "rock":
+        print (user_choice, "<", computer_choice, "YOU LOSE! :(")
+else:
+        print ("ERROR, YOU SCREWED UP IDIOT!")      
+
+
+
+
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN!")
