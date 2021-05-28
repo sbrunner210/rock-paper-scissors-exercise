f7ebad2fb5184ef78801972c779ba838ec4f7178
sbrunner210 <sb6911@stern.nyu.edu>
2021-05-28 16:17:23 -0400

    Deliverable

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # rock-paper-scissors-exercise
-First Class Deliverable
+## First Class Deliverable (5/30/2021)
+
+A game of "Rock, Paper, Scissors" for the user to compete against the computer.
+
+## Prerequisites
+
+  + Anaconda 3.7+
+  + Python 3.7+
+  + Pip
+
+## Installation
+
+Navigate to the app's folder from the command line.:
+
+```sh
+cd rock-paper-scissors-exercise
+```
+
+Use Anaconda to create and activate a new virtual environment, perhaps called "my-first-env":
+
+```sh
+conda create -n my-first-env python=3.8
+conda activate my-first-env
+```
+
+After activating the virtual environment, install package dependencies (see the ["requirements.txt"](/requirements.txt) file):
+
+```sh
+pip install -r requirements.txt
+```
+
+## Setup
+
+In in the root directory of your local repository, create a new file called ".env", and update the contents of the ".env" file to specify your desired username:
+
+    user ="ENTER PLAYER NAME"
+    
+## Usage
+
+Run the game script:
+
+```py
+python game.py
+```
+
+> NOTE: if you see an error like "ModuleNotFoundError: No module named '...'", it's because the given package isn't installed, so run the `pip` command above to ensure that package has been installed into the virtual environment.
+
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 # game.py
 
+# Importing random for the computer's choice.
 import random
+# Importing os and dotenv to properly utilze the .env file.
 import os
 import dotenv
 
@@ -10,18 +12,18 @@ dotenv.load_dotenv()
 # Pulling the information from the .env file.
 player_name = os.getenv("user")
 
-print ("Welcome",player_name,"to Rock, Paper, Scissors!")
+print (f"Welcome {player_name} to Rock, Paper, Scissors!")
 
 print("-----------------------------")
 print("Rock, Paper, Scissors, Shoot!")
 print("-----------------------------")
 
+# Prompting the user to make an input.
 user_choice = input("Please choose one of 'rock' , 'paper', 'scissors': ")
 
 print("-----------------------------")
 print("USER CHOICE: ",user_choice)
 print("-----------------------------")
-
 
 # validate the input such that only if it is one of the accepted inputs...
 #... we will continue with the rest of the program. Otherwise, we'll stop ...
@@ -29,11 +31,12 @@ print("-----------------------------")
 #... to run again
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print ("GOOD CHOICE!")
+    print ("Good choice!")
 else:
-    print("OOPS, INVALID INPUT. PLEASE TRY AGAIN.")
+    print("Oops! Invalid input. Please try again.")
     exit()
 
+# Lists the options for the computer to choose from. Uses random choice to select computer's choice. This needs to be imported.
 valid_options = ["rock","paper","scissors"]
 computer_choice = random.choice(valid_options)
 
@@ -41,27 +44,27 @@ print("-----------------------------")
 print("COMPUTER CHOICE: ", computer_choice)
 print("-----------------------------")
 
+# This is the logic for the game.
 if user_choice == computer_choice:
     print ("IT'S A TIE!")
 elif user_choice == "rock":
     if computer_choice == "scissors":
-        print (user_choice, ">", computer_choice, "YOU WIN!")
+        print (f"{user_choice} > {computer_choice} YOU WIN!")
     if computer_choice == "paper":
-        print (user_choice, "<", computer_choice, "YOU LOSE! :(")
+        print (f"{user_choice} < {computer_choice} YOU LOSE! :(")
 elif user_choice == "paper":
     if computer_choice == "rock":
-        print (user_choice, ">", computer_choice, "YOU WIN!")
+        print (f"{user_choice} > {computer_choice} YOU WIN!")
     if computer_choice == "scissors":
-        print (user_choice, "<", computer_choice, "YOU LOSE! :(")
+        print (f"{user_choice} < {computer_choice} YOU LOSE! :(")
 elif user_choice == "scissors":
     if computer_choice == "paper":
-        print (user_choice, ">", computer_choice, "YOU WIN!")
+        print (f"{user_choice} > {computer_choice} YOU WIN!")
     if computer_choice == "rock":
-        print (user_choice, "<", computer_choice, "YOU LOSE! :(")
+        print (f"{user_choice} < {computer_choice} YOU LOSE! :(")
 else:
-        print ("ERROR")      
+        print ("ERROR")  
+        exit()    
 
-
-
-
-print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN!")
+# End of script.
+print("This is the end of our game. Please play again!")
